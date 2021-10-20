7289ae5992e5d69ee4a035caede477e311ec5e0b
Krishnamrajuu <92290988+Krishnamrajuu@users.noreply.github.com>
2021-10-20 12:35:30 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,19 @@
+
+	printf(" age of stud/* C program to print details of student */
+#include<stdio.h>
+int main()
+{
+	// Variable Declaration
+	int age;
+	float  cgpa;
+	char initial;
+	
+	// Initialization
+	age = 21;
+	cgpa = 7.6;
+	initial = 'M';
+	
+	// Outputent is %d\n",age);
+	printf(" cgpa of student is %f\n",cgpa);
+	printf(" initial of student is %c\n",initial);
+}
